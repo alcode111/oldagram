8b6278a92c625767f966c83ac4c6bdef1f8a83b3
Ismail <141173099+alcode111@users.noreply.github.com>
2024-01-20 16:37:40 +0100

    Select the main container and append to it the innerHTML for the oldagram post to render the posts dynamically

--- index.js
+++ index.js
@@ -27,3 +27,49 @@ const posts = [
         likes: 152
     }
 ]
+
+const postContainer = document.getElementById("post-container")
+
+for (let i = 0; i < posts.length; i++) {
+    const oldaPost = document.createElement("div")
+    oldaPost.innerHTML = 
+    `
+    <header>
+    <div class="container header-container">
+        <span class="app-name">Oldagram</span>
+        <span class="profile-img-container"><img src="images/profile.png" alt="user picture" class="profile-pic"></span>
+    </div>
+</header>
+
+<main>
+    <section class="name-location">
+        <div class="container name-location-container">
+            <span class="avatar-van-gogh-container"><img src="${posts[i].avatar}" alt="avatar of van gogh" class="avatar-van-gogh"></span>
+            <div class="name-location-small-container">
+                <p class="painter-name">${posts[i].name}</p>
+                <p class="painter-location">${posts[i].location}</p>
+            </div>
+        </div>
+    </section>
+
+    <section class="main-image-section">
+        <img src="${posts[i].post}" alt="van gogh self-portrait" class="main-image">
+    </section>
+
+    <section class="bottom-section">
+        <div class="container bottom-section-container">
+            <div class="three-icons-container">
+                <img src="images/icon-heart.png" alt="heart like icon" class="icon heart">
+                <img src="images/icon-comment.png" alt="bubble comment icon" class="icon bubble">
+                <img src="images/icon-dm.png" alt="paper plane dm icon" class="icon dm">
+            </div>
+
+            <div class="likes-number">${posts[i].likes} likes</div>
+
+            <div class="comments"><span class="name">${posts[i].username}</span> <span class="comment">${posts[i].comment}</span></div>
+        </div>
+    </section>
+</main>
+    `
+    postContainer.append(oldaPost)
+}
